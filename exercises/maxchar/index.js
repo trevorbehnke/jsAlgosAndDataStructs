a4e33b28f404f36198d702a2c31fec34924a6747
trevor <me@trevorbehnke.com>
2021-08-10 20:40:59 -0700

    passed maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (i of str) {
+    if (charMap[i]) {
+      charMap[i]++;
+    } else {
+      charMap[i] = 1;
+    }
+  }
+
+  for (i in charMap) {
+    if (charMap[i] > max) {
+      max = charMap[i];
+      maxChar = i;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
