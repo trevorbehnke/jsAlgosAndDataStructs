21c8e67df9667b05432b7add1ed9579a1418e6c3
trevor <me@trevorbehnke.com>
2021-08-10 19:56:30 -0700

    passed reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const revInt = n.toString().split("").reverse().join("");
+
+  return n < 0 ? parseInt(revInt) * -1 : parseInt(revInt);
+}
 
 module.exports = reverseInt;
