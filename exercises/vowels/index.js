cd5969c1404b63134c17149ddef42ea45f1f9789
trevor <me@trevorbehnke.com>
2021-08-15 17:04:23 -0700

    soved fibonacci iteration & recurssion

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0;
+  const vowelArr = ["a", "e", "i", "o", "u"];
+  for (let i of str) {
+    if (vowelArr.includes(i)) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = vowels;
